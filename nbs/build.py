8621b7f870b4e9ecddd505eba6170ccf12446a0f
koaning <vincentwarmerdam@gmail.com>
2026-06-17 11:57:49 +0200

    Make notebook export order deterministic

--- nbs/build.py
+++ nbs/build.py
@@ -4,12 +4,11 @@ from pathlib import Path
 
 # Lets move out the code first
 
-order = InternalApp(app).execution_order
-codes = {k: v.code for k, v in InternalApp(app).graph.cells.items() if v.language=="python" and "## Export" in v.code}
+cells = InternalApp(app).graph.cells
+codes = [v.code for v in cells.values() if v.language=="python" and "## Export" in v.code]
 
 code_export = ""
-for i in order:
-    if i in codes:
-        code_export += codes[i].replace("## Export", "") + "\n"
+for code in codes:
+    code_export += code.replace("## Export", "") + "\n"
 
 Path("dicekit/__init__.py").write_text(code_export)
